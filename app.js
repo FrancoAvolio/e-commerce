3770367eaadf0e7868f1922c301c2b48d65e6329
FrancoAvolio <francoavolio@hotmail.com>
2023-04-22 16:04:26 -0300

    entrega final

--- app.js
+++ app.js
@@ -3,7 +3,50 @@ const verCarrito = document.getElementById("verCarrito");
 const modalContainer = document.getElementById("modalContainer")
 const cantidadCarrito = document.getElementById("cantidadCarrito")
 
-productos.forEach((product) => {
+const getProducts = async() => {
+const response = await fetch("data.json");
+const data = await response.json();
+    data.forEach((product) => {
+    let content = document.createElement("div");
+    content.className = "card";
+    content.innerHTML = `
+    <img src= "${product.img}">
+    <h3> ${product.nombre} </h3
+    <p class="price"> ${product.precio} $</p>
+    `;
+shopContent.append(content)
+let comprar = document.createElement ("button")
+comprar.innerText = "Agregar al carrito";
+comprar.className = "comprar"
+content.append(comprar)
+
+comprar.addEventListener("click",() => {
+    const repeat = carrito.some((repeatProduct) => repeatProduct.id === product.id);
+     if (repeat) {
+        carrito.map((prod) =>{
+            if (prod.id===product.id) {
+                prod.cantidad++
+            };
+        });
+    } else {
+    carrito.push({
+        id: product.id,
+        img: product.img,
+        nombre: product.nombre,
+        precio: product.precio,
+        cantidad: product.cantidad,
+    });
+    console.log(carrito);
+    console.log(carrito.length)
+    carritoCounter();
+    saveLocal();
+    }
+});
+});
+};
+getProducts()
+
+data.forEach((product) => {
     let content = document.createElement("div");
     content.className = "card";
     content.innerHTML = `
