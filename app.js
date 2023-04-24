faeefb611f9f2bfa0d380536034d365d7fe2ef1e
FrancoAvolio <francoavolio@hotmail.com>
2023-04-24 14:39:35 -0300

    Final

--- app.js
+++ app.js
@@ -46,44 +46,6 @@ comprar.addEventListener("click",() => {
 };
 getProducts()
 
-data.forEach((product) => {
-    let content = document.createElement("div");
-    content.className = "card";
-    content.innerHTML = `
-    <img src= "${product.img}">
-    <h3> ${product.nombre} </h3
-    <p class="price"> ${product.precio} $</p>
-    `;
-shopContent.append(content)
-let comprar = document.createElement ("button")
-comprar.innerText = "Agregar al carrito";
-comprar.className = "comprar"
-content.append(comprar)
-
-comprar.addEventListener("click",() => {
-    const repeat = carrito.some((repeatProduct) => repeatProduct.id === product.id);
-     if (repeat) {
-        carrito.map((prod) =>{
-            if (prod.id===product.id) {
-                prod.cantidad++
-            };
-        });
-    } else {
-    carrito.push({
-        id: product.id,
-        img: product.img,
-        nombre: product.nombre,
-        precio: product.precio,
-        cantidad: product.cantidad,
-    });
-    console.log(carrito);
-    console.log(carrito.length)
-    carritoCounter();
-    saveLocal();
-    }
-});
-});
-
 //Storage
 const saveLocal = () => {
 localStorage.setItem("carrito",JSON.stringify(carrito));
--- form.js
+++ form.js
@@ -6,9 +6,23 @@ formularioCompra.addEventListener("submit", (event) => {
   const direccion = document.getElementById("direccion").value;
   const tarjeta = document.getElementById("tarjeta").value;
 
+  localStorage.setItem("nombre", nombre);
+  localStorage.setItem("direccion", direccion);
+  localStorage.setItem("tarjeta", tarjeta);
+
   swal({
-    title: "Tu compra fue realizada con exito",
+    title: "Tu compra fue realizada con éxito",
     text: "A la brevedad recibirás el producto en el domicilio indicado",
     icon: "success"
-  })
+  });
 });
+
+const nombreGuardado = localStorage.getItem("nombre");
+const direccionGuardada = localStorage.getItem("direccion");
+const tarjetaGuardada = localStorage.getItem("tarjeta");
+
+if (nombreGuardado && direccionGuardada && tarjetaGuardada) {
+  document.getElementById("nombre").value = nombreGuardado;
+  document.getElementById("direccion").value = direccionGuardada;
+  document.getElementById("tarjeta").value = tarjetaGuardada;
+}
